446cf0e8a72eae2100093c0ca6b9798e905f5e2d
olivialee <oliviasylee@gmail.com>
2023-02-22 12:33:05 -0500

    Add join form and route for them

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,13 @@
 const router = require('express').Router();
 const { User } = require('../models');
 
-
 router.get('/', async(req, res) => {
     res.render('home');
+});
+
+router.get('/join', async(req, res) => {
+    console.log('join route called');
+    res.render('join');
 });
 
 module.exports = router;
