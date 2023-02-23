abe2c60d707d3db46b604778926b65f7be421bdb
olivialee <oliviasylee@gmail.com>
2023-02-22 22:13:03 -0500

    Add event listener to join form

--- public/js/join.js
+++ public/js/join.js
@@ -1,24 +1,26 @@
-const loginFormHandler = async (event) => {
+const signupFormHandler = async (event) => {
     event.preventDefault();
   
+    const username = document.querySelector('#typeNameX').value.trim();
     const email = document.querySelector('#typeEmailX').value.trim();
     const password = document.querySelector('#typePasswordX').value.trim();
   
-    if (email && password) {
-      const response = await fetch('/api/users/login', {
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, email, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
-        alert('Failed to log in.');
+        alert('Failed to sign up.');
       }
     }
   };
   
 document
-.querySelector('.login-form')
-.addEventListener('submit', loginFormHandler);
+    .querySelector('.signUp')
+    .addEventListener('submit', signupFormHandler);
+  
